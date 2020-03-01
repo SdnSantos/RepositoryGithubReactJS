97705ce3f0f37b18a6863c115bf69e2ec0e6c9d7
Sidney Santos <sidney_ssantos@hotmail.com>
2020-03-01 14:31:33 -0300

    Listando repositórios

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -72,3 +72,26 @@ export const SubmitButton = styled.button.attrs(props => ({
       }
     `}
 `;
+
+export const List = styled.ul`
+  list-style: nome;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    /* essa condicional pega o elemento atual e verifica se tem outra li, ou seja, vai aplicar a estilização a partir da segunda li, pois ela terá uma li anterior */
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
+`;
